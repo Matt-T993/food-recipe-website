59a7aaf62420f2247ec84151c5b8ab556f342caf
matthew theseira <matt_.t993@hotmail.com>
2024-02-19 17:40:00 +1100

    done the recipe page

--- src/pages/Recipe.jsx
+++ src/pages/Recipe.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import RecipeService from "../service/service";
 import "./pages.css";
+import RecipeBook from "../components/ui/RecipeBook";
+import RecipePage from "../components/ui/RecipePage";
 
 const Recipe = () => {
   const [recipe, setRecipe] = useState(null);
@@ -18,7 +20,6 @@ const Recipe = () => {
 
   useEffect(() => {
     getRecipe();
-    
   }, [id]);
 
   const getIngredients = () => {
@@ -27,7 +28,8 @@ const Recipe = () => {
       const ingredientKey = `strIngredient${i}`;
       const measureKey = `strMeasure${i}`;
       if (recipe[ingredientKey] && recipe[ingredientKey].trim() !== "") {
-        const ingredient = `${recipe[measureKey]} ${recipe[ingredientKey]}`.trim();
+        const ingredient =
+          `${recipe[measureKey]} ${recipe[ingredientKey]}`.trim();
         ingredients.push(ingredient);
       } else {
         break;
@@ -46,40 +48,46 @@ const Recipe = () => {
   const ingredients = getIngredients();
   const steps = formatInstructions(recipe.strInstructions);
 
-
   return (
     <div className="container">
       <div className="row">
-        <div className="recipe__content">
-          <div className="recipe__content--left">
-            <h2 className="recipe__content--name">{recipe.strMeal}</h2>
+        <div className="recipe__wrapper">
+          <div className="recipe__content">
+            <div className="recipe__content--title">
+              <h2 className="recipe__content--name">{recipe.strMeal}</h2>
+            </div>
+            <img
+              src={recipe.strMealThumb}
+              className="recipe__content--img"
+              alt=""
+            />
             <p className="recipe__content--info">
               Area: {recipe.strArea} | Category: {recipe.strCategory} | Tags:{" "}
               {recipe.strTags ? recipe.strTags : "No tags"}
             </p>
-            <hr/>
-            <h3 className="recipe__content--subtitle">Ingredients</h3>
-            <ul className="ingredients">
-              {ingredients.map((ingredient, index) => (
-                <div className="ingredient__wrapper" key={index}>
-                  <li className="ingredient__item">{ingredient}</li>
-                </div>
-              ))}
-            </ul>
-            <hr/>
-            <h3 className="recipe__content--subtitle">Instructions</h3>
-            <ol className="steps">
-              {steps.map((step, index) => (
-                <li className="step" key={index}>
-                  {index + 1} . {step}
-                </li>
-              ))}
-            </ol>
-          </div>
-          <div className="recipe__content--right">
-            <hr/>
-            <img src={recipe.strMealThumb} className="recipe__content--img" alt="" />
-            <hr/>
+            <div className="recipe__info">
+              <div className="recipe__info--ingredients">
+                <h3 className="recipe__content--subtitle">Ingredients</h3>
+                <ul className="ingredients">
+                  {ingredients.map((ingredient, index) => (
+                    <div className="ingredient__wrapper" key={index}>
+                      <li className="ingredient__item">{ingredient}</li>
+                    </div>
+                  ))}
+                </ul>
+              </div>
+              <div className="recipe__info--steps">
+                <h3 className="recipe__content--subtitle">Instructions</h3>
+                <ol className="steps">
+                  {steps.map((step, index) => (
+                    <li className="step" key={index}>
+                      <span className="number">{index + 1}</span>
+                      {step}
+                    </li>
+                  ))}
+                </ol>
+              </div>
+            </div>
           </div>
         </div>
       </div>
